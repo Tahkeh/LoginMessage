ccbf5d09614afb138d0f5b49a7e3c5e9d5580cfc
Fabian Neundorf <CommodoreFabianus@gmx.de>
2011-08-28 16:04:04 +0200

    *implement BPU for LM;

--- src/com/tahkeh/loginmessage/SListener.java
+++ src/com/tahkeh/loginmessage/SListener.java
@@ -3,35 +3,22 @@ package com.tahkeh.loginmessage;
 import org.bukkit.event.server.PluginDisableEvent;
 import org.bukkit.event.server.PluginEnableEvent;
 import org.bukkit.event.server.ServerListener;
-import org.bukkit.plugin.Plugin;
 
-public class SListener extends ServerListener //Etc, etc, for iConomy
+// Reacts on plugin en-/disable to update the handlers.
+public class SListener extends ServerListener
 {
-    private Main plugin;
-
-    public SListener(Main plugin) {
-        this.plugin = plugin;
-    }
 
     @Override
     public void onPluginDisable(PluginDisableEvent event) {
-        if (Main.iConomy != null) {
-            if (event.getPlugin().getDescription().getName().equals("iConomy")) {
-                Main.Permissions = null;
-            }
-        }
+        Main.getEconomy().unload(event.getPlugin());
+        Main.getPermissions().unload(event.getPlugin());
+        Main.getEconomy().load();
+        Main.getPermissions().load();
     }
 
     @Override
     public void onPluginEnable(PluginEnableEvent event) {
-        if (Main.iConomy == null) {
-            Plugin iConomy = plugin.getServer().getPluginManager().getPlugin("iConomy");
-
-            if (iConomy != null && plugin.iConomyEnabled() == true) {
-                if (iConomy.isEnabled()) {
-                    Main.iConomy = (com.iConomy.iConomy) iConomy;
-                }
-            }
-        }
+        Main.getEconomy().load(event.getPlugin());
+        Main.getPermissions().load(event.getPlugin());
     }
 }
--- src/com/tahkeh/loginmessage/entries/Group.java
+++ src/com/tahkeh/loginmessage/entries/Group.java
@@ -2,21 +2,24 @@ package com.tahkeh.loginmessage.entries;
 
 import org.bukkit.entity.Player;
 
-import com.nijiko.permissions.PermissionHandler;
+import de.xzise.wrappers.permissions.PermissionsHandler;
 
-public class Group extends DefaultEntry
-{
-    private final PermissionHandler handler;
+public class Group extends DefaultEntry {
+	private final PermissionsHandler handler;
 
-    public Group(String group, PermissionHandler handler) {
-        super(group);
-        this.handler = handler;
-    }
+	public Group(String group, PermissionsHandler handler) {
+		super(group);
+		this.handler = handler;
+	}
 
-    public boolean match(Player player) {
-        //TODO: Maybe use Permissions 3 calls
-    	String group = this.handler.getGroup(player.getWorld().getName(), player.getName()); 
-        return group == null ? false : group.equalsIgnoreCase(this.value);
-    }
+	public boolean match(Player player) {
+		String[] groups = this.handler.getGroup(player.getWorld().getName(), player.getName());
+		for (String group : groups) {
+			if (group.equalsIgnoreCase(this.value)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 }
