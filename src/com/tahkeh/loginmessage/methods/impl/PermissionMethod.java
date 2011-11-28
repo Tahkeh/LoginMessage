acc3336d0d9cb9f5ec9be94e78499a82cd83455c
Fabian Neundorf <CommodoreFabianus@gmx.de>
2011-11-28 21:46:04 +0100

    *triple instead of tripple;

--- src/com/tahkeh/loginmessage/methods/impl/PermissionMethod.java
+++ src/com/tahkeh/loginmessage/methods/impl/PermissionMethod.java
@@ -4,7 +4,7 @@ import org.bukkit.entity.Player;
 
 import com.tahkeh.loginmessage.methods.OriginalPlayerMethod;
 
-import de.xzise.bukkit.util.callback.CallbackTripple;
+import de.xzise.bukkit.util.callback.CallbackTriple;
 import de.xzise.wrappers.permissions.Permission;
 import de.xzise.wrappers.permissions.PermissionsHandler;
 
@@ -12,17 +12,17 @@ public class PermissionMethod<T extends Object> extends OriginalPlayerMethod {
 
 	private final PermissionsHandler handler;
 	private final Permission<T> permission;
-	private final CallbackTripple<String, PermissionsHandler, Permission<T>, Player> valueCallback;
+	private final CallbackTriple<String, PermissionsHandler, Permission<T>, Player> valueCallback;
 
 	public static final PlayerStringCallback PLAYER_STRING_CALLBACK = new PlayerStringCallback();
-	public static class PlayerStringCallback implements CallbackTripple<String, PermissionsHandler, Permission<String>, Player> {
+	public static class PlayerStringCallback implements CallbackTriple<String, PermissionsHandler, Permission<String>, Player> {
 		@Override
 		public String call(PermissionsHandler handler, Permission<String> permission, Player player) {
 			return handler.getString(player, permission);
 		}
 	}
 
-	public PermissionMethod(final PermissionsHandler handler, final Permission<T> permission, CallbackTripple<String, PermissionsHandler, Permission<T>, Player> valueCallback) {
+	public PermissionMethod(final PermissionsHandler handler, final Permission<T> permission, CallbackTriple<String, PermissionsHandler, Permission<T>, Player> valueCallback) {
 		this.handler = handler;
 		this.permission = permission;
 		this.valueCallback = valueCallback;
